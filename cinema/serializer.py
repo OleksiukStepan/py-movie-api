49afbca57050eec6cd023964a0a873d66c19fd00
Stepan <oleksiukstepann@gmail.com>
2024-09-27 00:06:28 +0300

    change inherit MovieSerializer from ModelSerializer -> Serializer & remove Meta class, add type hints

--- cinema/serializer.py
+++ cinema/serializer.py
@@ -3,20 +3,16 @@ from rest_framework import serializers
 from cinema.models import Movie
 
 
-class MovieSerializer(serializers.ModelSerializer):
+class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=255)
+    title = serializers.CharField(max_length=100)
     description = serializers.CharField()
     duration = serializers.IntegerField()
 
-    class Meta:
-        model = Movie
-        fields = ["id", "title", "description", "duration"]
-
-    def create(self, validated_data):
+    def create(self, validated_data: dict) -> Movie:
         return Movie.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
+    def update(self, instance: Movie, validated_data: dict) -> Movie:
         instance.title = validated_data.get("title", instance.title)
         instance.description = validated_data.get(
             "description", instance.description
